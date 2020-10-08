2772da9be77d014725fa4e5ac1606c85fffdd29b
ayuberlianda <71421525+ayuberlianda@users.noreply.github.com>
2020-10-08 12:06:12 +0700

    Add files via upload

---
+++ tugas_if_perbandingan.php
@@ -0,0 +1,14 @@
+<?php
+
+	$a = 80;
+	$b = 10;
+
+	if($a < $b){
+		echo $a, "Lebih Kecil Dari ", $b;
+	}else if($a > $b){
+		echo $a, "Lebih Besar Dari ", $b;
+	}else{
+		echo  $a,"Sama Dengan",$b;
+	}
+	
+?>
